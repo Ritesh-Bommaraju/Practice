2c03398bedcb31f3ed75f9c19b2f5d0ec3acd9c5
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-03-13 12:52:53 +0530

    Time: 58 ms (31.95%), Space: 25.9 MB (55.63%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
-    int calculateSquare(int number)
-    {
-        return number * number;
-    }
+    // int calculateSquare(int number)
+    // {
+    //     return number * number;
+    // }
+    // vector<int> sortedSquares(vector<int>& nums) {
+    //     for(int i = 0; i < nums.size(); i++){
+    //         nums[i] = calculateSquare(nums[i]);
+    //     }
+    //     sort(nums.begin(), nums.end());
+    //     return nums;
+    // }
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i = 0; i < nums.size(); i++){
-            nums[i] = calculateSquare(nums[i]);
+        // two pointer method
+        int left = 0;
+        int right = nums.size() - 1;
+        vector<int> ans(nums.size());
+        int pointer = nums.size() - 1;
+        while(left <= right){
+            if(abs(nums[left]) > abs(nums[right])){
+                ans[pointer] = nums[left]*nums[left];
+                left++;
+            }
+            else{
+                ans[pointer] = nums[right]*nums[right];
+                right--;
+            }
+            pointer--;
         }
-        sort(nums.begin(), nums.end());
-        return nums;
-        
+        return ans;
     }
 };

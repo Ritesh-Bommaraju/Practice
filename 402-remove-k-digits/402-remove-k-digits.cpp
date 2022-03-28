d73ba17ef1c8ceda56f7001df9c6dc7e1611572b
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-03-28 22:37:02 +0530

    Time: 16 ms (25.71%), Space: 8.7 MB (18.86%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -3,15 +3,27 @@ public:
     string removeKdigits(string num, int k) {
         int n = num.size();
         stack<char> st;
-        for(auto c: num){
-            while(!st.empty() && k>0 && st.top()>c){   
+        for(int i = 0; i<n; i++){
+            while(!st.empty() && k>0 && st.top()>num[i]){   
                 st.pop();      
                 k-=1;   
             }
-            if(!st.empty() || c!='0'){
-                st.push(c);
-            }            
+            if(!st.empty() || num[i]!='0'){
+                st.push(num[i]);
+            }   
         }
+        
+//         for(auto c: num){
+//             while(!st.empty() && k>0 && st.top()>c){   
+//                 st.pop();      
+//                 k-=1;   
+//             }
+//             if(!st.empty() || c!='0'){
+//                 st.push(c);
+//             }            
+//         }
+        // for cases like 123 .... no poping here at all so at last we have to delete 
+        //elements from the back then.
         while(!st.empty() && k--){
             st.pop();
         }

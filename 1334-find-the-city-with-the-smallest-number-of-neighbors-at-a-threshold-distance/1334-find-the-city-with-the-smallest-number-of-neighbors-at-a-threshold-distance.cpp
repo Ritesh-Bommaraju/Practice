618dec0785406519724c818b57a791a525655380
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-03-29 22:49:24 +0530

    Time: 127 ms (43.87%), Space: 12.5 MB (63.98%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int limit) {
+        //floyyd warshal
+        vector<vector<int>>dist(n, vector<int>(n, INT_MAX));
+	for (int i = 0; i < n; i++) {
+		dist[i][i] = 0;
+	}
+
+	for (vector<int>v : edges)
+	{
+		dist[v[0]][v[1]] = v[2];
+		dist[v[1]][v[0]] = v[2];
+	}
+
+	for (int k = 0; k < n; k++) //pivot
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX && dist[i][k] + dist[k][j] < dist[i][j]) {
+					dist[i][j] = dist[i][k] + dist[k][j];
+				}
+			}
+		}
+	}
+
+	int holidaycount;
+	int ans;
+	for (int i = 0; i < n; i++)
+	{	int count = 0;
+		for (int j = 0; j < n; j++)
+		{
+			if (i != j && dist[i][j] <= limit)
+				count++;
+		}
+
+		if (count <= holidaycount)
+		{
+			ans = i;
+			holidaycount = count;
+
+		}
+	}
+	return ans;
+        
+        
+        
+    }
+};

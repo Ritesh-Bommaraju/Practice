180838e9e290f28da7ed585d9e17de88c5607232
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-04-09 00:44:18 +0530

    Time: 92 ms (65.93%), Space: 27.4 MB (96.27%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,4 +1,5 @@
-class Solution {
+/*class Solution {
+bfs sol
 public:
     vector<int> DIR = {0, 1, 0, -1, 0};
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
@@ -16,6 +17,33 @@ public:
                 if (nr < 0 || nr == m || nc < 0 || nc == n || mat[nr][nc] != -1) continue;
                 mat[nr][nc] = mat[r][c] + 1;
                 q.emplace(nr, nc);
+            }
+        }
+        return mat;
+    }
+};*/
+
+class Solution {
+    // dp sol
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>> &mat) {
+        int m = mat.size(), n = mat[0].size(), INF = m + n; // The distance of cells is up to (M+N)
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (mat[r][c] == 0) continue;
+                int top = INF, left = INF;
+                if (r - 1 >= 0) top = mat[r - 1][c];
+                if (c - 1 >= 0) left = mat[r][c - 1];
+                mat[r][c] = min(top, left) + 1;
+            }
+        }
+        for (int r = m - 1; r >= 0; r--) {
+            for (int c = n - 1; c >= 0; c--) {
+                if (mat[r][c] == 0) continue;
+                int bottom = INF, right = INF;
+                if (r + 1 < m) bottom = mat[r + 1][c];
+                if (c + 1 < n) right = mat[r][c + 1];
+                mat[r][c] = min(mat[r][c], min(bottom, right) + 1);
             }
         }
         return mat;

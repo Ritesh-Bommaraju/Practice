cefb415fcc220f65d1235759ce77f6ec4702897e
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-04-11 23:00:11 +0530

    Time: 30 ms (76.64%), Space: 13.9 MB (87.46%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -18,4 +18,9 @@ public:
         return ans;
         
     }
+//     If k is 1 then new column is (j+1) and if k is k then new column is (j+k) and new column should in range [0,m-1]. So new column is (j+k)%m.
+
+// For row, we have to first find the number of rows added to previous row which is equal to the number of times (j+k) exceeds m. So, number of rows added is (j+k)/m.
+
+// So, new row is (i + (j+k)/m) and new row should in range [0,n-1]. So new column is (i + (j+k)/m)%n.
 };

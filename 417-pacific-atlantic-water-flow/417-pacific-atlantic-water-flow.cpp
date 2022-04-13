9cf995ca64b11b69cd32af720940823d4eecbca7
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-04-13 12:28:53 +0530

    Time: 46 ms (78.67%), Space: 24.1 MB (21.11%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -11,17 +11,27 @@ public:
         m = size(mat), n = size(mat[0]);
         atlantic = pacific = vector<vector<bool> >(m, vector<bool>(n, false));
 		// perform dfs from all edge cells (ocean-connected cells)
-        for(int i = 0; i < m; i++) dfs(mat, pacific, i, 0), dfs(mat, atlantic, i, n - 1);
-        for(int i = 0; i < n; i++) dfs(mat, pacific, 0, i), dfs(mat, atlantic, m - 1, i);             
+        for(int i = 0; i < m; i++){
+            dfs(mat, pacific, i, 0);
+            dfs(mat, atlantic, i, n - 1);
+        }
+        for(int i = 0; i < n; i++){ 
+            dfs(mat, pacific, 0, i);
+            dfs(mat, atlantic, m - 1, i); 
+        }            
         return ans;
     }
     
     void dfs(vector<vector<int> >& mat, vector<vector<bool> >& visited, int i, int j){        
-        if(visited[i][j]) return;
+        if(visited[i][j]){
+            return;
+        }
         visited[i][j] = true;
 		// if cell reachable from both the oceans, insert into final answer vector
-        if(atlantic[i][j] && pacific[i][j]) ans.push_back(vector<int>{i, j});    
-		// dfs from current cell only if height of next cell is greater
+        if(atlantic[i][j] && pacific[i][j]){
+            ans.push_back(vector<int>{i, j});
+        }
+		// dfs from current cell only if height of next cell is greater or equal
         if(i + 1 <  m && mat[i + 1][j] >= mat[i][j]) dfs(mat, visited, i + 1, j); 
         if(i - 1 >= 0 && mat[i - 1][j] >= mat[i][j]) dfs(mat, visited, i - 1, j);
         if(j + 1 <  n && mat[i][j + 1] >= mat[i][j]) dfs(mat, visited, i, j + 1); 

f965bbf43fe7a2f61c4998ec5bace9806ed0aff7
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-04-14 22:38:31 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (24.52%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    
-    int rec(vector<int>& nums, int i, int j){
+    int rec(vector<int>& nums, int i, int j, vector<vector<int>>&dp){
         if(i > j){
             return 0;
         }
-        int choice1 = nums[i] + min(rec(nums,i+2,j), rec(nums,i+1, j-1));
-        int choice2 = nums[j] + min(rec(nums,i+1,j-1), rec(nums,i, j-2));
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int choice1 = nums[i] + min(rec(nums,i+2,j,dp), rec(nums,i+1, j-1,dp));
+        int choice2 = nums[j] + min(rec(nums,i+1,j-1,dp), rec(nums,i, j-2,dp));
         
-        return max(choice1, choice2);
+        return dp[i][j] = max(choice1, choice2);
     }
     
     
@@ -16,7 +18,8 @@ public:
     bool PredictTheWinner(vector<int>& nums) {
         int sum = accumulate(nums.begin(), nums.end(), 0);
         int n = nums.size();
-        int best_score = rec(nums, 0, n-1);
+        vector<vector<int>> dp(n, vector<int>(n,-1));
+        int best_score = rec(nums, 0, n-1,dp);
         if(sum - best_score > best_score){
             return false;
         }

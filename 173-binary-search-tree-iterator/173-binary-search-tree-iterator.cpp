2ecfa342304f65e085a06a108585830dcec76e18
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-04-21 01:17:17 +0530

    Time: 52 ms (20.94%), Space: 24.2 MB (56.67%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -9,34 +9,58 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class BSTIterator {
-public:
+// class BSTIterator {
+// public:
     
-    vector<int> nums;
-    int ptr; 
+//     vector<int> nums;
+//     int ptr; 
     
-    void inorder(TreeNode* root){
-        if(root==NULL) return ;
-        inorder(root->left);
-        nums.push_back(root->val);
-        inorder(root->right);
+//     void inorder(TreeNode* root){
+//         if(root==NULL) return ;
+//         inorder(root->left);
+//         nums.push_back(root->val);
+//         inorder(root->right);
         
-    }
+//     }
     
-    BSTIterator(TreeNode* root) {
-      inorder(root);  
-      ptr=0;
-    }
+//     BSTIterator(TreeNode* root) {
+//       inorder(root);  
+//       ptr=0;
+//     }
     
-    int next() {
-        return nums[ptr++];
-    }
+//     int next() {
+//         return nums[ptr++];
+//     }
     
+//     bool hasNext() {
+//          return nums.size()>ptr;
+//     }
+// };
+class BSTIterator {
+    stack<TreeNode *> myStack;
+public:
+    BSTIterator(TreeNode *root) {
+        pushAll(root);
+    }
+
+    /** @return whether we have a next smallest number */
     bool hasNext() {
-         return nums.size()>ptr;
+        return !myStack.empty();
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode *tmpNode = myStack.top(); // take top element
+        myStack.pop(); // pop it out
+        pushAll(tmpNode->right); // if has a right, call pushall ...
+        return tmpNode->val; // return the top value
+    }
+
+private:
+    void pushAll(TreeNode *node) {
+        for (; node != NULL; myStack.push(node), node = node->left);
     }
 };
-
 
 /**
  * Your BSTIterator object will be instantiated and called as such:

3d025ddc7d825363edac4f4a28e77d567aba86fe
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-05-02 21:35:50 +0530

    Time: 49 ms (19.11%), Space: 19.3 MB (58.81%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int snowBallSize = 0; 
+        for (int i=0;i<nums.size();i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
+            }
+            else if (snowBallSize > 0) {
+                swap(nums[i], nums[i-snowBallSize]);
+            }
+        }
+    }
+};

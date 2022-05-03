326ce35c3ff37e3c7825c1ce0e552e6fed7f4b29
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-05-04 01:38:26 +0530

    Time: 75 ms (73.82%), Space: 48.4 MB (76.87%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        int reachable = 0;
-        for(int i = 0; i < n; i++){
-            if(i > reachable){
-                return false;
+        // int n = nums.size();
+        // int reachable = 0;
+        // for(int i = 0; i < n; i++){
+        //     if(i > reachable){
+        //         return false;
+        //     }
+        //     reachable = max(reachable, nums[i] + i);
+        // }
+        // return true;
+        int lastgoodindex = nums.size()-1;
+        for(int i = nums.size()-1; i>=0; i--){
+            if(i + nums[i] >= lastgoodindex){
+                lastgoodindex = i;
             }
-            reachable = max(reachable, nums[i] + i);
         }
-        return true;
-        
+        if(lastgoodindex == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 };

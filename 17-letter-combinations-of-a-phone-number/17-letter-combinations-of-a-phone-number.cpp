8cbc47621430452bb5cff88d17970d3da6c72dda
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-05-09 20:00:39 +0530

    Time: 2 ms (44.02%), Space: 6.6 MB (60.00%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     void helper(vector<string>& ans, string digits, vector<string>& keypad, int index, string curr_string){
+        // if the index we reach is equal to the length of the given digits string length, i.e we exhausted the whole string so we append it to the ans vector
         if(index == digits.size()){
             ans.push_back(curr_string);
             return;
         }
+        // we extract the correct index of the digit we are correctly in and extract the value at that index in the keypad vector;
         int d = digits[index] - '0';
         for(char ch : keypad[d]){
+            // we increment the index by 1 and add the char to the current string
             helper(ans, digits, keypad, index + 1, curr_string + ch);
         }
     }

ddd2170ee2c58e377c1ffd98d849dec9e448e34d
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-05-14 11:50:04 +0530

    Time: 231 ms (72.50%), Space: 86 MB (34.99%) - LeetHub

--- 853-car-fleet/853-car-fleet.cpp
+++ 853-car-fleet/853-car-fleet.cpp
@@ -1,24 +1,5 @@
 class Solution {
 public:
-//     vector<pair<int,double>> v;
-//         double t;
-//         for(int i=0; i<position.size(); i++){
-//             t = (double)(target-position[i])/speed[i];
-//             v.push_back({position[i], t});
-//             //cout << t << " ";
-//         }
-        
-//         sort(v.begin(), v.end(), greater<pair<int,double>>());
-//         stack<double> s;
-//         for(auto p: v){
-//             //cout << p.first << " ";
-//             while(!s.empty() && s.top() >= p.second){
-//                 s.pop();
-//             }
-//             s.push(p.second);
-//         }
-        
-//         return s.size();
     int carFleet(int target, vector<int>& pos, vector<int>& speed){
         if (pos.empty() || speed.empty()) return 0;
         vector<pair<int, double>> intervals; // start position, time to reach target;
@@ -26,15 +7,16 @@ public:
             intervals.push_back(make_pair(pos[i], (double)(target - pos[i])/(double)(speed[i])));
         }
         sort(intervals.begin(), intervals.end());
-        int ans = 1;
         int n = pos.size();
-        double slowest = intervals[n-1].second;
-        for(int i = n-2; i>=0; i--){
-            if(intervals[i].second > slowest){
-                ans++;
-                slowest = intervals[i].second;
+        stack<double> st;
+        for(int i = n-1; i>=0; i--){
+            if(st.empty() or intervals[i].second > st.top()){
+                st.push(intervals[i].second);
             }
         }
-        return ans;
+        return st.size();
     }
+    // if a car behind me takes more time than me to reach target, itll be of a seperate fleet, else
+    // all other cars which take less time than me, behind me , form a fleet with me.....
+    
 };

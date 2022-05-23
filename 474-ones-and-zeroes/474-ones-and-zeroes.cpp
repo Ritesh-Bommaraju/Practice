3e95e294ef68c1ea9ee9cfa2dd828206b62f3aab
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-05-24 02:05:28 +0530

    Time: 262 ms (80.29%), Space: 88.9 MB (28.12%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    int findMaxForm(vector<string>& strs, int m, int n) {
-        vector<vector<int> > dp(n + 1, vector<int>(m + 1, 0));
-        int ones, zeros;
-        for (auto str : strs) {
-            ones = count(str.begin(), str.end(), '1');
-            zeros = count(str.begin(), str.end(), '0');
-            for (int i = n; i >= ones; i--) {
-                for (int j = m; j >= zeros; j--) {
-                    
-                    // In each cell, we check the max between using the current string or not.
-                    // dp[i][j] is the previous value in the cell - not using the current str.
-                    // dp[i - ones][j - zeros] is the value if we want to use the current string.
-                    // we add one because we are using another string.
-                    
-                    dp[i][j] = max(dp[i][j], dp[i - ones][j - zeros] + 1);
-                }
-            }
-        }
-        return dp[n][m];
-        
+    vector<vector<vector<int>>> dp;
+    int findMaxForm(vector<string>& strs, int m, int n) {      
+        dp.resize(m+1,vector<vector<int>>(n+1,vector<int>(strs.size(),-1)));
+        return findmax(strs,m,n,0);
     }
+    int findmax (vector<string>& strs, int m, int n,int index){
+         if(index==strs.size())return 0;
+         if(dp[m][n][index]!=-1)return dp[m][n][index];
+        
+         int countzeroes = count(strs[index].begin(),strs[index].end(),'0');
+         int countones = strs[index].size()-countzeroes;
+         
+         if(m-countzeroes>=0 && n-countones>=0){
+             return dp[m][n][index]= max(1+findmax(strs,m-countzeroes,n-countones,index+1),findmax(strs,m,n,index+1));
+         }
+         return dp[m][n][index]=findmax(strs,m,n,index+1);
+     }
+
 };

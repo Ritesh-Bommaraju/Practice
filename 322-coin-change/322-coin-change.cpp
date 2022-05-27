9e7ad81b10e0ab50c634ac03c58520156c91baea
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-05-27 09:52:07 +0530

    Time: 182 ms (22.87%), Space: 14.3 MB (68.35%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        if(amount==0) return 0;
-        if(coins.size()==1) return amount%coins[0]==0 ? amount/coins[0] : -1;
-        queue<int> bfs;
-        int count = 0;
-        bfs.push(amount);
+        vector<int> dp(amount+1, amount+1);
+        dp[0] = 0;
         
-        unordered_set<int> done; 
-        
-        while(!bfs.empty()){
-           int size = bfs.size();
-            for(int i =0 ;i<size; i++){
-                auto curr = bfs.front();
-                bfs.pop();
-                for(auto i : coins){
-                    if(curr-i == 0) return count+1;
-                    else if(done.find(curr-i)!=done.end()) continue;
-                    else if(curr-i > 0) {
-                        bfs.push(curr-i);
-                        done.insert(curr-i);
-                    }
-                    else continue;
+        // dp[i] represents the fewest number of coins to make up the amount i
+        for (int i = 1; i <= amount; i++) {
+            
+            for (auto coin : coins) {
+                
+                if (coin <= i) {
+                    
+                    dp[i] = min(dp[i], dp[i-coin]+1);
                 }
             }
-            count++;
         }
         
-        return -1;
+        return dp[amount] > amount ? -1 : dp[amount];
         
     }
 };

f7bba78ba9c22409f4570a0f0711046735b604c1
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-05-28 13:32:25 +0530

    Time: 151 ms (58.08%), Space: 66.8 MB (47.41%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
     int findLengthOfShortestSubarray(vector<int>& arr) {
-        int left = 0;
-        while(left + 1 < arr.size() && arr[left] <= arr[left+1]){
-            left++;
+        // int left = 0;
+        // while(left + 1 < arr.size() && arr[left] <= arr[left+1]){
+        //     left++;
+        // }
+        // if(left == arr.size() - 1){
+        //     return 0;
+        // }
+        // int right = arr.size() - 1;
+        // while(right > left && arr[right] >= arr[right-1]){
+        //     right--;
+        // }
+        // int temp = arr.size() - left - 1;
+        // int ans = min(temp, right);
+        // int i = 0;
+        // int j = right;
+        // while(i <= left && j < arr.size()){
+        //     if(arr[i] <= arr[j]){
+        //         ans = min(ans, j - i - 1);
+        //         i++;
+        //     }
+        //     else{
+        //         j++;
+        //     }
+        // }
+        // return ans;
+        int n = arr.size();
+        int l = 0, r = n - 1;
+        int res = n;
+        
+        while (r - 1 >= 0 && arr[r - 1] <= arr[r]) {
+            r--;
         }
-        if(left == arr.size() - 1){
-            return 0;
-        }
-        int right = arr.size() - 1;
-        while(right > left && arr[right] >= arr[right-1]){
-            right--;
-        }
-        int temp = arr.size() - left - 1;
-        int ans = min(temp, right);
-        int i = 0;
-        int j = right;
-        while(i <= left && j < arr.size()){
-            if(arr[i] <= arr[j]){
-                ans = min(ans, j - i - 1);
-                i++;
+        
+        res = r;
+        
+        while (l < r && (l == 0 || arr[l - 1] <= arr[l])) {
+            while (r < n && arr[l] > arr[r]) {
+                r++;
             }
-            else{
-                j++;
-            }
+            
+            res = min(res, r - l - 1);
+            l++;
         }
-        return ans;
+        
+        return res;
     }
 };

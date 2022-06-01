40841b2e8a17081b7677bac55f45317d0d59c972
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-06-01 13:16:35 +0530

    Time: 1115 ms (26.36%), Space: 174.9 MB (79.39%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,30 +1,54 @@
 class Solution {
 public:
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        disc = vector<int>(n);
-        low = vector<int>(n);
-        for (auto conn : connections) {
-            edgeMap[conn[0]].push_back(conn[1]);
-            edgeMap[conn[1]].push_back(conn[0]);
+    void dfs(int node, int parent, vector<int> &tin, vector<int> &low, vector<int> &vis, vector<int> graph[], vector<vector<int>> &ans, int &time)
+    {
+        //when we visite node first time, tin and low of the node are equal to the time
+        tin[node]=low[node]=time++;
+        vis[node] = 1; //mark node as visited
+        
+        for(auto it : graph[node])
+        {
+            if(it == parent) continue; //to avoid backtracking
+            
+            //if node is not visited, call the dfs function
+            if(!vis[it])
+            {
+                dfs(it, node, tin, low, vis, graph, ans, time);
+                
+                //when we return, low of the node is equal to the minimum of low of its child and low of itself
+                low[node] = min(low[node], low[it]);
+                
+                //when we get low of child is greater than tin of the node
+                //it means there is only path to cover child that's why {node, it} will be our critical connections or bridge 
+                if(low[it] > tin[node])
+                    ans.push_back({node, it});
+            }
+            
+            //if node is already visited and tin of the child is less than the low of itself
+            //we assign tin of the child to the low of the node
+            else
+                low[node] = min(low[node], tin[it]);
+            
         }
-        dfs(0, -1);
+    }
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) 
+    {
+        //tin - time of insertion at the node
+        //low - lowest time of insertion at the node
+        //vis - mark visited node 
+        vector<int> tin(n, -1), low(n, -1), vis(n, 0);
+        vector<vector<int>> ans;
+        vector<int> graph[n]; //stores connections in the form of graph
+        int time=0;
+        
+        for(auto it: connections)
+        {
+            graph[it[0]].push_back(it[1]);
+            graph[it[1]].push_back(it[0]);
+        }
+        
+        //call dfs function
+        dfs(0, -1, tin, low, vis, graph, ans, time);
         return ans;
     }
-    void dfs(int curr, int prev) {
-        disc[curr] = low[curr] = time++;
-        for (int next : edgeMap[curr]) {
-            if (disc[next] == 0) {
-                dfs(next, curr);
-                low[curr] = min(low[curr], low[next]);
-            } else if (next != prev)
-                low[curr] = min(low[curr], disc[next]);
-            if (low[next] > disc[curr]) 
-                ans.push_back({curr, next});
-        }
-    }
-private:
-    vector<int> disc{0}, low{0};
-    int time = 1;
-    vector<vector<int>> ans;
-    unordered_map<int, vector<int>> edgeMap;
 };

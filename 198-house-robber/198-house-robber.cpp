df11197f38c8df66cd5519a1147018badd8e2d49
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-06-20 21:18:20 +0530

    Time: 3 ms (53.01%), Space: 7.8 MB (46.86%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-//     int n;
-//     int helper(vector<int>&nums,int curr, vector<int>& dp){
-//         if(curr >= nums.size()){
-//             return 0;
-//         }
-//         if(dp[curr] != -1){
-//             return dp[curr];
-//         }
-//         // max(rec(nums, idx+1, dp), nums[idx] + rec(nums, idx+2, dp));
-//         return dp[curr] = max( (nums[curr] + helper(nums, curr + 2, dp)) ,helper(nums, curr+1,dp));
-        
-//     }
-    
-//     int rob(vector<int>& nums) {
-//         n = nums.size();
-//         vector<int> dp(n+1,-1);
-//         return helper(nums,0,dp);
-//     }
-    int rob(vector<int>& nums) {
-        int prev = 0, prev2 = 0, curr = 0;
-        for(int a : nums){
-            curr = max(prev, prev2 + a);
-            prev2 = prev;
-            prev = curr;
+    int n;
+    int helper(vector<int>&nums,int curr, vector<int>& dp){
+        if(curr >= nums.size()){
+            return 0;
         }
-        return curr;
+        if(dp[curr] != -1){
+            return dp[curr];
+        }
+        return dp[curr] = max( (nums[curr] + helper(nums, curr + 2, dp)) ,helper(nums, curr+1,dp));
+        
     }
+    
+    int rob(vector<int>& nums) {
+        n = nums.size();
+        vector<int> dp(n+1,-1);
+        return helper(nums,0,dp);
+    }
+//     int rob(vector<int>& nums) {
+//         int prev = 0, prev2 = 0, curr = 0;
+//         for(int a : nums){
+//             curr = max(prev, prev2 + a);
+//             prev2 = prev;
+//             prev = curr;
+//         }
+//         return curr;
+//     }
 };

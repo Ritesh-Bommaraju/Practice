fc6af30817ad3f277c9d02fff7733f70c9885977
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-06-20 15:35:27 +0530

    Time: 554 ms (6.44%), Space: 14.6 MB (88.61%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,23 +1,54 @@
+
+// Sort all the words in descending order of their lengths.
+// Initialise res with first word in the vector + "#"
+// Now we just need to find, if other words in the vector + # are substring of res or not.
+// if not, append that word to res.
+bool compare(string &a,string &b){
+    return a.length()>b.length();
+}
 class Solution {
 public:
+    
     int minimumLengthEncoding(vector<string>& words) {
-        unordered_set<string> s;
-        for(string w : words){
-            s.insert(w);
+        
+        sort(words.begin(),words.end(),compare);
+        string res="";
+        res=res+words[0]+"#";
+        
+        for(int i=1;i<words.size();i++){
+            
+            int pos=res.find(words[i]+"#");
+            if(pos!=-1)
+                continue;
+            else
+                res+=words[i]+"#";
+            
         }
-        for(string word : s){
-            for(int i = 1; i < word.size(); i++){
-                // cout << word.substr(i) << endl;
-                s.erase(word.substr(i));
-            }
-        }
-        // for(string ar : s){
-        //     cout << ar << " ";
-        // }
-        int ans = 0;
-        for(string ar : s){
-            ans += ar.size() + 1;
-        }
-        return ans;
+        
+        return res.length();
+        
     }
 };
+// class Solution {
+// public:
+//     int minimumLengthEncoding(vector<string>& words) {
+//         unordered_set<string> s;
+//         for(string w : words){
+//             s.insert(w);
+//         }
+//         for(string word : s){
+//             for(int i = 1; i < word.size(); i++){
+//                 // cout << word.substr(i) << endl;
+//                 s.erase(word.substr(i));
+//             }
+//         }
+//         // for(string ar : s){
+//         //     cout << ar << " ";
+//         // }
+//         int ans = 0;
+//         for(string ar : s){
+//             ans += ar.size() + 1;
+//         }
+//         return ans;
+//     }
+// };

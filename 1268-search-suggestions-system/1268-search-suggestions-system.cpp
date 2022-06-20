be6621c0244d3233243acb6610e4058114973128
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-06-20 09:26:36 +0530

    Time: 1396 ms (7.10%), Space: 332.1 MB (11.18%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
-    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+    // vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+    //     string curr = "";
+    //     vector<vector<string>> ans;
+    //     sort(products.begin(), products.end());
+    //     for(auto c : searchWord){
+    //         curr += c;
+    //         set<string> s;
+    //         for(auto p: products){
+    //             if(p.substr(0,curr.size()) == curr){
+    //                 s.insert(p);
+    //             }
+    //         }
+    //         vector<string> temp;
+    //         int count = 0;
+    //         while(!s.empty()){
+    //             if(count == 3){
+    //                 break;
+    //             }
+    //             temp.push_back(*s.begin());
+    //             s.erase(s.begin());
+    //             count++;
+    //         }
+    //         ans.push_back(temp);
+    //     }
+    //     return ans;
+    // }
+        vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
         string curr = "";
         vector<vector<string>> ans;
         sort(products.begin(), products.end());
         for(auto c : searchWord){
             curr += c;
-            set<string> s;
+            vector<string> temp;
             for(auto p: products){
                 if(p.substr(0,curr.size()) == curr){
-                    s.insert(p);
+                    temp.push_back(p);
                 }
-            }
-            vector<string> temp;
-            int count = 0;
-            while(!s.empty()){
-                if(count == 3){
+                if(temp.size() == 3){
                     break;
                 }
-                temp.push_back(*s.begin());
-                s.erase(s.begin());
-                count++;
             }
             ans.push_back(temp);
         }

57c99f6e6092b0112b0273ffcdb2ae863df01dd1
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-06-24 20:03:24 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (96.02%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        for(char c : s){
+        for(auto c : s){
             if(c == '('|| c == '{' || c == '['){
                 st.push(c);
             }

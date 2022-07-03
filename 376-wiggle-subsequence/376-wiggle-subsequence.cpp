33fb854e529d27408588d6f4d666c0e7c06cdab6
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-07-03 12:16:13 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (75.45%) - LeetHub

---
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1){
+            return 1;
+        }
+        int prev_diff = nums[1] - nums[0];
+        int count = 1;
+        if(prev_diff != 0){
+            count = 2;
+        }
+        for(int i = 2; i < n; i++){
+            int curr_diff = nums[i] - nums[i-1];
+            if((curr_diff > 0 && prev_diff <= 0)||(curr_diff < 0 && prev_diff >= 0)){
+                count++;
+                prev_diff = curr_diff;
+            }
+        }
+        return count;   
+        
+    }
+};

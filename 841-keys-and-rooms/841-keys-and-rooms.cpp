a93fe9caea5d6ff7c002b109ac4c37aeecddf612
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-07-10 17:02:14 +0530

    Time: 16 ms (46.43%), Space: 10.4 MB (78.92%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,24 +1,51 @@
+// class Solution {
+// public:
+
+//     void helper(vector<vector<int>> &graph, int curr, vector<bool> &vis){
+//         vis[curr] = true;
+//         for(int i = 0; i < graph[curr].size(); i++){
+//             if(!vis[graph[curr][i]]){
+//                 helper(graph, graph[curr][i], vis);
+//             }
+//         }
+//     }
+
+//     bool canVisitAllRooms(vector<vector<int>>& rooms) {
+//         int n=rooms.size();
+//         vector<bool> vis(n, false);
+//         helper(rooms, 0, vis);
+//         for(int i = 0; i < n; i++){
+//             if(!vis[i]){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
-
-    void helper(vector<vector<int>> &graph, int curr, vector<bool> &vis){
-        vis[curr] = true;
-        for(int i = 0; i < graph[curr].size(); i++){
-            if(!vis[graph[curr][i]]){
-                helper(graph, graph[curr][i], vis);
-            }
-        }
-    }
-
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n=rooms.size();
-        vector<bool> vis(n, false);
-        helper(rooms, 0, vis);
+        vector<bool> visited(n, false);
+        queue<int> q;
+        q.push(0);
+        while(!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            visited[curr] = true;
+            for (int k : rooms[curr]){
+                if (visited[k] == false){
+                    q.push(k);
+                }   
+            } 
+        }
         for(int i = 0; i < n; i++){
-            if(!vis[i]){
+            if(!visited[i]){
                 return false;
             }
         }
         return true;
+        
     }
 };

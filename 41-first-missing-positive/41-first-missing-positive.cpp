1be604ac72bf54277cfbe637a7885e0d005c33b9
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-07-14 01:26:39 +0530

    Time: 222 ms (66.23%), Space: 83 MB (77.72%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -2,16 +2,46 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
+        if(n == 1){
+            if(nums[0] == 1){
+                return 2;
+            }
+            else{
+                return 1;
+            }
+        }
+        bool containsone = false;
+        
+        // for one it is an edge case
+        
+        // step 1 : remove all numbers out of the range 1->n
         for(int i = 0; i < n; i++){
-            while(nums[i] >= 1 and nums[i] < n and nums[i] != nums[nums[i] -1]){
-                swap(nums[i], nums[nums[i] - 1]);
+            if(nums[i] == 1){
+                containsone = true;
+            }
+            if(nums[i] <= n && nums[i] >= 1){
+                continue;
+            }
+            else{
+                nums[i] = 1;
+            }
+        }
+        if(containsone == false){
+            return 1;
+        }
+        // make the given array as visited only 
+        for(int i = 0; i < n; i++){
+            int idx = abs(nums[i]) - 1;
+            if(nums[idx] > 0){
+                nums[idx] = -nums[idx];
             }
         }
         for(int i = 0; i < n; i++){
-            if(i+1 != nums[i]){
-                return i+1;
+            if(nums[i] > 0){
+                return i + 1;
             }
         }
         return n+1;
+        
     }
 };

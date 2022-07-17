8e5c1679b417e13f63c6597c29f4e374bc4b879f
Ritesh Bommaraju <85054067+Ritesh-Bommaraju@users.noreply.github.com>
2022-07-17 18:30:40 +0530

    Time: 56 ms (37.49%), Space: 16.3 MB (53.19%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
+    // int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+    //     if(startFuel > target){
+    //         return 0;
+    //     }
+    //     priority_queue<int> pq;
+    //     int max_we_can_go = startFuel;
+    //     int idx = 0;
+    //     int stops = 0;
+    //     while(max_we_can_go < target){
+    //         while(idx < stations.size() and stations[idx][0] <= max_we_can_go){
+    //             pq.push(stations[idx][1]);
+    //             idx++;
+    //         }
+    //         if(pq.empty() == true){
+    //             return -1;
+    //         }
+    //         max_we_can_go += pq.top();
+    //         pq.pop();
+    //         stops++;
+    //     }
+    //     return stops;
+    // }
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
-        if(startFuel > target){
-            return 0;
-        }
+        int idx=0,stock = 0,ans=0;
         priority_queue<int> pq;
-        int max_we_can_go = startFuel;
-        int idx = 0;
-        int stops = 0;
-        while(max_we_can_go < target){
-            while(idx < stations.size() and stations[idx][0] <= max_we_can_go){
+        pq.push(startFuel);
+        while(!pq.empty())
+        {
+            int currFuel = pq.top();
+            pq.pop();
+            stock+=currFuel;
+            if(stock >= target)
+                return ans;
+            while(idx < stations.size() and stock >= stations[idx][0])
+            {
                 pq.push(stations[idx][1]);
                 idx++;
             }
-            if(pq.empty() == true){
-                return -1;
-            }
-            max_we_can_go += pq.top();
-            pq.pop();
-            stops++;
+            ans++;
         }
-        return stops;
+        return -1;
     }
 };
